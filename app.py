bf9e29d6a24786c945b7a280176de94213846e72
joao camilo <jlf.camilo@icloud.com>
2024-11-21 19:31:36 -0400

    feat: new response.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['TESTING'] = True 
+
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, !</p>"
+    return "<p>Ocean Brasil!</p>"
 
 if __name__ == "__main__":
     app.run(debug=True)
